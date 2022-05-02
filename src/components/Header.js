31ca3779276e8a0b7ace703430f903a9069ae66d
Gaston Roxas <gacaro51597@gmail.com>
2022-05-02 20:13:14 +0800

    completed css for navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,15 @@
 function Header() {
-  return <h1>Header component</h1>;
+  return (
+    <header className="header">
+      <img
+        className="header--image"
+        src={require("../images/troll-face.png")}
+        alt="troll face"
+      />
+      <h2 className="header--title">Header Component</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
 }
 
 export default Header;
